47fea32d53c7b221e9581b68eae849014d51851c
vishalmrode <vishalmrode@outlook.com>
2025-04-02 20:39:20 +0530

    Refactor Playwright configuration to clean up mobile viewport test comments

--- playwright.config.js
+++ playwright.config.js
@@ -58,7 +58,7 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
     },
 
-    /* Test against mobile viewports. */ //test
+    /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
