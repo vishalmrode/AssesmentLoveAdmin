6d57067b547cffb8bc9ae213055036134ca68c2d
vishalmrode <vishalmrode@outlook.com>
2025-04-02 20:38:10 +0530

    Update Playwright configuration to include mobile viewport test comments

--- playwright.config.js
+++ playwright.config.js
@@ -58,7 +58,7 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
     },
 
-    /* Test against mobile viewports. */
+    /* Test against mobile viewports. */ //test
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
